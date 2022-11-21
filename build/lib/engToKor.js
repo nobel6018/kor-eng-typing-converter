7647c0d32c992cfab9be81658c9072759d562c9e
Lee, YoungHoon <nobel6018@gmail.com>
2022-11-21 23:07:17 +0900

    v1.2.3

--- build/lib/engToKor.js
+++ build/lib/engToKor.js
@@ -1,54 +1,63 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function engToKor(text) {
-    return text.replaceAll(/\w+/gi, function (s) { return convertEngToKor(s); });
+    var engToKorConverter = new EngToKorConverter();
+    return text.replaceAll(/\w+/gi, function (s) { return engToKorConverter.convertEngStringToKor(s); });
 }
 exports.default = engToKor;
-function convertEngToKor(text) {
-    var vowelSubstitutedText = substituteVowels(text);
-    var firstConsonantsSubstitutedText = substituteFirstConsonants(vowelSubstitutedText);
-    var lastConsonantsSubstitutedText = substituteLastConsonants(firstConsonantsSubstitutedText);
-    var normalizedText = lastConsonantsSubstitutedText.normalize('NFKC');
-    var theLeftVowelsSubstitutedText = substituteTheLeftVowels(normalizedText);
-    return substituteTheLeftConsonants(theLeftVowelsSubstitutedText);
-}
-function substituteVowels(text) {
-    return text
-        .replaceAll(/([rsefaqtdwczxvg])(hk|ho|hl|nj|np|nl|ml)/gi, function (s, g1, g2) { return g1.toLowerCase() + vowelDict[g2.toLowerCase()]; })
-        .replaceAll(/([rsefaqtdwczxvg])([OP])/gi, function (s, g1, g2) {
-        if (g2 === 'O' || g2 === 'P') {
-            return g1.toLowerCase() + vowelDict[g2];
-        }
-        else {
-            return s;
-        }
-    })
-        .replaceAll(/([rsefaqtdwczxvg])([koijpuhynbml])/gi, function (s, g1, g2) { return g1 + vowelDict[g2.toLowerCase()]; });
-}
-function substituteFirstConsonants(text) {
-    return text
-        .replaceAll(/([REQTW])([ㅏ-ㅣ])/g, function (s, g1, g2) { return firstConsonantDict[g1] + g2; })
-        .replaceAll(/([a-zA-Z])([ㅏ-ㅣ])/gi, function (s, g1, g2) { return firstConsonantDict[g1.toLowerCase()] + g2; });
-}
-function substituteLastConsonants(text) {
-    return text
-        .replaceAll(/([ㅏ-ㅣ])([RT])([^ㅏ-ㅣ]|$)/g, function (s, g1, g2, g3) { return g1 + lastConsonantDict[g2] + g3; })
-        .replaceAll(/([ㅏ-ㅣ])(rt|sw|sg|fr|fa|fq|ft|fx|fv|fg|qt)([^ㅏ-ㅣ]|$)/gi, function (s, g1, g2, g3) { return g1 + lastConsonantDict[g2.toLowerCase()] + g3; })
-        .replaceAll(/([ㅏ-ㅣ])([rsefaqtdwczxvg])([^ㅏ-ㅣ]|$)/gi, function (s, g1, g2, g3) { return g1 + lastConsonantDict[g2.toLowerCase()] + g3; });
-}
-function substituteTheLeftConsonants(text) {
-    return text
-        .replaceAll(/[REQTW]/g, function (s) { return compatibleConsonantDict[s]; })
-        .replaceAll(/([a-zA-Z])/g, function (s) {
-        return compatibleConsonantDict[s.toLowerCase()] ? compatibleConsonantDict[s.toLowerCase()] : s;
-    });
-}
-function substituteTheLeftVowels(text) {
-    return text
-        .replaceAll(/hk|ho|hl|nj|np|nl|ml/gi, function (s) { return vowelDict[s.toLowerCase()]; })
-        .replaceAll(/[OP]/g, function (s) { return vowelDict[s]; })
-        .replaceAll(/[a-zA-Z]/g, function (s) { return (vowelDict[s.toLowerCase()] ? vowelDict[s.toLowerCase()] : s); });
-}
+var EngToKorConverter = /** @class */ (function () {
+    function EngToKorConverter() {
+        this.text = '';
+    }
+    EngToKorConverter.prototype.convertEngStringToKor = function (text) {
+        this.text = text;
+        this.substituteVowels();
+        this.substituteFirstConsonants();
+        this.substituteLastConsonants();
+        this.text = this.text.normalize('NFKC');
+        this.substituteTheLeftVowels();
+        this.substituteTheLeftConsonants();
+        return this.text;
+    };
+    EngToKorConverter.prototype.substituteVowels = function () {
+        this.text = this.text
+            .replaceAll(/([rsefaqtdwczxvg])(hk|ho|hl|nj|np|nl|ml)/gi, function (s, g1, g2) { return g1.toLowerCase() + vowelDict[g2.toLowerCase()]; })
+            .replaceAll(/([rsefaqtdwczxvg])([OP])/gi, function (s, g1, g2) {
+            if (g2 === 'O' || g2 === 'P') {
+                return g1.toLowerCase() + vowelDict[g2];
+            }
+            else {
+                return s;
+            }
+        })
+            .replaceAll(/([rsefaqtdwczxvg])([koijpuhynbml])/gi, function (s, g1, g2) { return g1 + vowelDict[g2.toLowerCase()]; });
+    };
+    EngToKorConverter.prototype.substituteFirstConsonants = function () {
+        this.text = this.text
+            .replaceAll(/([REQTW])([ㅏ-ㅣ])/g, function (s, g1, g2) { return firstConsonantDict[g1] + g2; })
+            .replaceAll(/([a-zA-Z])([ㅏ-ㅣ])/gi, function (s, g1, g2) { return firstConsonantDict[g1.toLowerCase()] + g2; });
+    };
+    EngToKorConverter.prototype.substituteLastConsonants = function () {
+        this.text = this.text
+            .replaceAll(/([ㅏ-ㅣ])([RT])([^ㅏ-ㅣ]|$)/g, function (s, g1, g2, g3) { return g1 + lastConsonantDict[g2] + g3; })
+            .replaceAll(/([ㅏ-ㅣ])(rt|sw|sg|fr|fa|fq|ft|fx|fv|fg|qt)([^ㅏ-ㅣ]|$)/gi, function (s, g1, g2, g3) { return g1 + lastConsonantDict[g2.toLowerCase()] + g3; })
+            .replaceAll(/([ㅏ-ㅣ])([rsefaqtdwczxvg])([^ㅏ-ㅣ]|$)/gi, function (s, g1, g2, g3) { return g1 + lastConsonantDict[g2.toLowerCase()] + g3; });
+    };
+    EngToKorConverter.prototype.substituteTheLeftConsonants = function () {
+        this.text = this.text
+            .replaceAll(/[REQTW]/g, function (s) { return compatibleConsonantDict[s]; })
+            .replaceAll(/([a-zA-Z])/g, function (s) {
+            return compatibleConsonantDict[s.toLowerCase()] ? compatibleConsonantDict[s.toLowerCase()] : s;
+        });
+    };
+    EngToKorConverter.prototype.substituteTheLeftVowels = function () {
+        this.text = this.text
+            .replaceAll(/hk|ho|hl|nj|np|nl|ml/gi, function (s) { return vowelDict[s.toLowerCase()]; })
+            .replaceAll(/[OP]/g, function (s) { return vowelDict[s]; })
+            .replaceAll(/[a-zA-Z]/g, function (s) { return (vowelDict[s.toLowerCase()] ? vowelDict[s.toLowerCase()] : s); });
+    };
+    return EngToKorConverter;
+}());
 // prettier-ignore
 var vowelDict = {
     hk: 'ㅘ', ho: 'ㅙ', hl: 'ㅚ', nj: 'ㅝ', np: 'ㅞ', nl: 'ㅟ', ml: 'ㅢ', k: 'ㅏ', o: 'ㅐ', i: 'ㅑ', O: 'ㅒ', j: 'ㅓ', p: 'ㅔ', u: 'ㅕ', P: 'ㅖ', h: 'ㅗ', y: 'ㅛ', n: 'ㅜ', b: 'ㅠ', m: 'ㅡ', l: 'ㅣ'
--- build/lib/korToEng.js
+++ build/lib/korToEng.js
@@ -1,35 +1,78 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var common_1 = require("../common");
-var keyMaps_1 = require("../keyMaps");
 function korToEng(text) {
-    var result = '';
-    var normalized = text.normalize('NFD');
-    for (var _i = 0, normalized_1 = normalized; _i < normalized_1.length; _i++) {
-        var char = normalized_1[_i];
-        var unicode = char.charCodeAt(0);
-        if ((0, common_1.isKorean)(unicode)) {
-            result += convertKoreaAlphabetToEnglishAlphabet(unicode);
-        }
-        else {
-            result += char;
-        }
-    }
-    return result;
+    var korToEngConverter = new KorToEngConverter();
+    return korToEngConverter.convertKorStringToEng(text);
 }
 exports.default = korToEng;
-function convertKoreaAlphabetToEnglishAlphabet(unicode) {
-    var koreaAlphabetType = (0, common_1.getKoreaAlphabetType)(unicode);
-    switch (koreaAlphabetType) {
-        case "\uCD08\uC131" /* KoreaAlphabetType.FIRST_CONSONANT_LETTER */:
-            return keyMaps_1.firstConsonantKeyMap.eng[unicode - 4352 /* KoreanUnicode.FIRST_CONSONANT_START */];
-        case "\uC911\uC131" /* KoreaAlphabetType.MIDDLE_VOWEL_LETTER */:
-            return keyMaps_1.middleVowelKeyMap.eng[unicode - 4449 /* KoreanUnicode.MIDDLE_VOWEL_START */];
-        case "\uC885\uC131" /* KoreaAlphabetType.LAST_CONSONANT_LETTER */:
-            return keyMaps_1.lastConsonantKeyMap.eng[unicode - 4520 /* KoreanUnicode.LAST_CONSONANT_START */];
-        case "\uD638\uD658\uC790\uC74C" /* KoreaAlphabetType.COMPATIBLE_CONSONANT_LETTER */:
-            return keyMaps_1.compatibleConsonantKeyMap.eng[unicode - 12593 /* KoreanUnicode.COMPATIBLE_CONSONANT_START */];
-        case "\uD638\uD658\uBAA8\uC74C" /* KoreaAlphabetType.COMPATIBLE_VOWEL_LETTER */:
-            return keyMaps_1.compatibleVowelKeyMap.eng[unicode - 12623 /* KoreanUnicode.COMPATIBLE_VOWEL_START */];
+var KorToEngConverter = /** @class */ (function () {
+    function KorToEngConverter() {
     }
-}
+    KorToEngConverter.prototype.convertKorStringToEng = function (text) {
+        var _this = this;
+        return text.normalize('NFD').replaceAll(/[ᄀ-ᇂㄱ-ㅣ]/g, function (s) { return _this.convertKorCharToEng(s); });
+    };
+    KorToEngConverter.prototype.convertKorCharToEng = function (char) {
+        var unicode = char.charCodeAt(0);
+        var koreaAlphabetType = this.getKoreaAlphabetType(unicode);
+        switch (koreaAlphabetType) {
+            case "\uCD08\uC131" /* KoreaAlphabetType.FIRST_CONSONANT_LETTER */:
+                return firstConsonantKeyMap.eng[unicode - 4352 /* KoreanUnicode.FIRST_CONSONANT_START */];
+            case "\uC911\uC131" /* KoreaAlphabetType.MIDDLE_VOWEL_LETTER */:
+                return middleVowelKeyMap.eng[unicode - 4449 /* KoreanUnicode.MIDDLE_VOWEL_START */];
+            case "\uC885\uC131" /* KoreaAlphabetType.LAST_CONSONANT_LETTER */:
+                return lastConsonantKeyMap.eng[unicode - 4520 /* KoreanUnicode.LAST_CONSONANT_START */];
+            case "\uD638\uD658\uC790\uC74C" /* KoreaAlphabetType.COMPATIBLE_CONSONANT_LETTER */:
+                return compatibleConsonantKeyMap.eng[unicode - 12593 /* KoreanUnicode.COMPATIBLE_CONSONANT_START */];
+            case "\uD638\uD658\uBAA8\uC74C" /* KoreaAlphabetType.COMPATIBLE_VOWEL_LETTER */:
+                return compatibleVowelKeyMap.eng[unicode - 12623 /* KoreanUnicode.COMPATIBLE_VOWEL_START */];
+        }
+    };
+    KorToEngConverter.prototype.getKoreaAlphabetType = function (unicode) {
+        if (unicode >= 4352 /* KoreanUnicode.FIRST_CONSONANT_START */ && unicode <= 4448 /* KoreanUnicode.FIRST_CONSONANT_END */) {
+            return "\uCD08\uC131" /* KoreaAlphabetType.FIRST_CONSONANT_LETTER */;
+        }
+        else if (unicode >= 4449 /* KoreanUnicode.MIDDLE_VOWEL_START */ && unicode <= 4519 /* KoreanUnicode.MIDDLE_VOWEL_END */) {
+            return "\uC911\uC131" /* KoreaAlphabetType.MIDDLE_VOWEL_LETTER */;
+        }
+        else if (unicode >= 4520 /* KoreanUnicode.LAST_CONSONANT_START */ && unicode <= 4546 /* KoreanUnicode.LAST_CONSONANT_END */) {
+            return "\uC885\uC131" /* KoreaAlphabetType.LAST_CONSONANT_LETTER */;
+        }
+        else if (unicode >= 12593 /* KoreanUnicode.COMPATIBLE_CONSONANT_START */ &&
+            unicode <= 12622 /* KoreanUnicode.COMPATIBLE_CONSONANT_END */) {
+            return "\uD638\uD658\uC790\uC74C" /* KoreaAlphabetType.COMPATIBLE_CONSONANT_LETTER */;
+        }
+        else if (unicode >= 12623 /* KoreanUnicode.COMPATIBLE_VOWEL_START */ && unicode <= 12643 /* KoreanUnicode.COMPATIBLE_VOWEL_END */) {
+            return "\uD638\uD658\uBAA8\uC74C" /* KoreaAlphabetType.COMPATIBLE_VOWEL_LETTER */;
+        }
+        else {
+            throw Error('It is not korea alphabet. Korean alphabet unicode is between 0x1100 and 0x11c2');
+        }
+    };
+    return KorToEngConverter;
+}());
+// prettier-ignore
+var firstConsonantKeyMap = {
+    kor: ['ᄀ', 'ᄁ', 'ᄂ', 'ᄃ', 'ᄄ', 'ᄅ', 'ᄆ', 'ᄇ', 'ᄈ', 'ᄉ', 'ᄊ', 'ᄋ', 'ᄌ', 'ᄍ', 'ᄎ', 'ᄏ', 'ᄐ', 'ᄑ', 'ᄒ'],
+    eng: ['r', 'R', 's', 'e', 'E', 'f', 'a', 'q', 'Q', 't', 'T', 'd', 'w', 'W', 'c', 'z', 'x', 'v', 'g'],
+};
+// prettier-ignore
+var middleVowelKeyMap = {
+    kor: ['ᅡ', 'ᅢ', 'ᅣ', 'ᅤ', 'ᅥ', 'ᅦ', 'ᅧ', 'ᅨ', 'ᅩ', 'ᅪ', 'ᅫ', 'ᅬ', 'ᅭ', 'ᅮ', 'ᅯ', 'ᅰ', 'ᅱ', 'ᅲ', 'ᅳ', 'ᅴ', 'ᅵ'],
+    eng: ['k', 'o', 'i', 'O', 'j', 'p', 'u', 'P', 'h', 'hk', 'ho', 'hl', 'y', 'n', 'nj', 'np', 'nl', 'b', 'm', 'ml', 'l'],
+};
+// prettier-ignore
+var lastConsonantKeyMap = {
+    kor: ['ᆨ', 'ᆩ', 'ᆪ', 'ᆫ', 'ᆬ', 'ᆭ', 'ᆮ', 'ᆯ', 'ᆰ', 'ᆱ', 'ᆲ', 'ᆳ', 'ᆴ', 'ᆵ', 'ᆶ', 'ᆷ', 'ᆸ', 'ᆹ', 'ᆺ', 'ᆻ', 'ᆼ', 'ᆽ', 'ᆾ', 'ᆿ', 'ᇀ', 'ᇁ', 'ᇂ'],
+    eng: ['r', 'R', 'rt', 's', 'sw', 'sg', 'e', 'f', 'fr', 'fa', 'fq', 'ft', 'fx', 'fv', 'fg', 'a', 'q', 'qt', 't', 'T', 'd', 'w', 'c', 'z', 'x', 'v', 'g'],
+};
+// prettier-ignore
+var compatibleConsonantKeyMap = {
+    kor: ['ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'],
+    eng: ['r', 'R', 'rt', 's', 'sw', 'sg', 'e', 'E', 'f', 'fr', 'fa', 'fq', 'ft', 'fx', 'fv', 'fg', 'a', 'q', 'Q', 'qt', 't', 'T', 'd', 'w', 'W', 'c', 'z', 'x', 'v', 'g'],
+};
+// prettier-ignore
+var compatibleVowelKeyMap = {
+    kor: ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'],
+    eng: ['k', 'o', 'i', 'O', 'j', 'p', 'u', 'P', 'h', 'hk', 'ho', 'hl', 'y', 'n', 'nj', 'np', 'nl', 'b', 'm', 'ml', 'l'],
+};
